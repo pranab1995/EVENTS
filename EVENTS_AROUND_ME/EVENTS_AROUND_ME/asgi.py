cc7cf71736808c08d14076ada3b984be5b113d5a
Pranab Kumar Moharana <pranabmoharana13@gmail.com>
2022-07-14 17:19:39 +0530

    Initial Commit

---
+++ EVENTS_AROUND_ME/EVENTS_AROUND_ME/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for EVENTS_AROUND_ME project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EVENTS_AROUND_ME.settings')
+
+application = get_asgi_application()
---
+++ EVENTS_AROUND_ME/EVENTS_AROUND_ME/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for EVENTS_AROUND_ME project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EVENTS_AROUND_ME.settings')
+
+application = get_wsgi_application()
